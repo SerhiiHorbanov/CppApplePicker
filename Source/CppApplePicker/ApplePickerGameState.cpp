c6fec8a2c040b581a10d3f338dc442991f2aaa9e
Serhiiko <serikgorbanev@gmail.com>
2026-02-07 07:56:44 +0100

    Implemented high score saving and loading

--- Source/CppApplePicker/ApplePickerGameState.cpp
+++ Source/CppApplePicker/ApplePickerGameState.cpp
@@ -3,6 +3,52 @@
 
 #include "ApplePickerGameState.h"
 
+#include "HighScoreSave.h"
+#include "Kismet/GameplayStatics.h"
+
+void AApplePickerGameState::LoadHighScore()
+{
+	const UHighScoreSave* Save = Cast<UHighScoreSave>(
+		UGameplayStatics::LoadGameFromSlot(TEXT("HighScore"), 0)
+	);
+
+	if (Save)
+	{
+		HighScore = Save->HighScore;
+	}
+	
+	GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Green, FString::Printf(TEXT("HighScore: %d"), HighScore));
+}
+
+void AApplePickerGameState::BeginPlay()
+{
+	Super::BeginPlay();
+
+	LoadHighScore();
+}
+
+void AApplePickerGameState::SaveHighScore()
+{
+	UHighScoreSave* Save = Cast<UHighScoreSave>(
+		UGameplayStatics::CreateSaveGameObject(UHighScoreSave::StaticClass())
+	);
+
+	Save->HighScore = Score;
+
+	UGameplayStatics::SaveGameToSlot(Save, TEXT("HighScore"), 0);
+}
+
+void AApplePickerGameState::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+	
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, FString::Printf(TEXT("HighScore on end: %d"), HighScore));
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, FString::Printf(TEXT("Score on end: %d"), Score));
+	
+	if (Score > HighScore)
+		SaveHighScore();
+}
+
 void AApplePickerGameState::IncreaseScore()
 {
 	Score += 100;
--- Source/CppApplePicker/ApplePickerGameState.h
+++ Source/CppApplePicker/ApplePickerGameState.h
@@ -6,17 +6,25 @@
 #include "GameFramework/GameStateBase.h"
 #include "ApplePickerGameState.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class CPPAPPLEPICKER_API AApplePickerGameState : public AGameStateBase
 {
 	GENERATED_BODY()
 	
+	UPROPERTY(EditAnywhere, Category="ApplePickerGameState")
 	int Score;
 
+	UPROPERTY(EditAnywhere, Category="ApplePickerGameState")
+	int HighScore;
+	
 public:
+	
+	void LoadHighScore();
+	
+	virtual void BeginPlay() override;
+	void SaveHighScore();
+
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 	
 	void IncreaseScore();
 };
